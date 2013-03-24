6705f848d9d3d15ff57ca21aae34307d54a3d6fa
Tomting <giuseppe.mastrangelo@tomting.com>
2013-03-24 07:38:16 +0100

    Update runalltest.sh

--- runalltest.sh
+++ runalltest.sh
@@ -23,3 +23,15 @@ export THREADS=100
 
 export THREADS=1000
 ./runtest.sh orion -p hosts=orion:localhost:9001:DEFAULT
+
+#####################################à####
+# COLLECT
+##########################################
+zip collect.zip orion.* redis.*
+
+#####################################à####
+# CLEANUP
+##########################################
+./cleanup.sh redis
+./cleanup.sh orion
+
