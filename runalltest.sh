6f1ff70b9b62263f41d5167ed5acfab3a443fb51
Tomting <giuseppe.mastrangelo@tomting.com>
2013-03-24 07:30:04 +0100

    Create runalltest.sh

---
+++ runalltest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################à####
+# REDIS
+##########################################
+export THREADS=1
+./runtest.sh redis -p redis.host=localhost
+
+export THREADS=100
+./runtest.sh redis -p redis.host=localhost
+
+export THREADS=1000
+./runtest.sh redis -p redis.host=localhost
+
+#####################################à####
+# ORION
+##########################################
+export THREADS=1
+./runtest.sh orion -p hosts=orion:localhost:9001:DEFAULT
+
+export THREADS=100
+./runtest.sh orion -p hosts=orion:localhost:9001:DEFAULT
+
+export THREADS=1000
+./runtest.sh orion -p hosts=orion:localhost:9001:DEFAULT
